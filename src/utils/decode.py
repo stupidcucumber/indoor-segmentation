c47115b645197fb4146a3085e7951b6350e80196
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2025-03-25 22:16:11 +0200

    chore: implemented decoding function

--- src/utils/decode.py
+++ src/utils/decode.py
@@ -1,3 +1,5 @@
+import cv2
+import numpy as np
 import torch
 
 
@@ -15,3 +17,55 @@ def decode(logits: torch.Tensor) -> torch.Tensor:
         Decoded outputs of the model.
     """
     return logits.argmax(dim=1, keepdim=True)
+
+
+def generate_color_map(nclasses: int = 151) -> dict[int, np.ndarray]:
+    """Generate color map for segmentation.
+
+    Parameters
+    ----------
+    nclasses : int
+        Number of classes to generate colord for.
+
+    Returns
+    -------
+    dict[int, np.ndarray]
+        Generated map of colors for classes.
+    """
+    result = {}
+
+    for class_id in range(nclasses):
+
+        result[class_id] = np.random.randint(0, 255, 3, dtype=np.uint8)
+
+    return result
+
+
+def decode_cv2(logits: torch.Tensor, cmap: dict[int, np.ndarray]) -> cv2.Mat:
+    """Decode model predictions to the image.
+
+    Parameters
+    ----------
+    logits : torch.Tensor
+        Outputs of the segmentation model.
+    cmap : dict[int, np.ndarray]
+        Map of colors for classes.
+
+    Returns
+    -------
+    cv2.Mat
+        Image of the segmentation.
+    """
+    decoded_tensor = decode(logits)[0][0]
+
+    result = np.zeros(
+        (decoded_tensor.shape[0], decoded_tensor.shape[1], 3), dtype=np.uint8
+    )
+
+    for y_index, y in enumerate(decoded_tensor):
+
+        for x_index, x in enumerate(y):
+
+            result[y_index, x_index] = cmap[x.cpu().item()]
+
+    return result
